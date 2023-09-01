a7b76c002b0b7da20c859d3696a23985860c90b8
osung_jihyun <61964114+Choi-Jihyun@users.noreply.github.com>
2023-09-01 13:15:14 +0900

    speakers 페이지 수정 완료

--- src/component/Speakerslist.js
+++ src/component/Speakerslist.js
@@ -25,6 +25,9 @@ export default function Speakerslist() {
   },[selectCategory,allProducts])
   
   const navigate=useNavigate()
+  // 아래 변수에 일관성 보완: isAllCategory, isHomeCategory 추가 삽입
+  const isAllCategory = selectCategory === 'ALL';
+  const isHomeCategory = selectCategory === 'HOME';
   const isPortableCategory = selectCategory === 'PORTABLE';
   const isLimitedCategory = selectCategory === 'LIMITED EDITION';
 
@@ -52,6 +55,49 @@ export default function Speakerslist() {
         <p>{selectCategory}</p>
       </div>
     
+
+
+    {/* 변경 시작 */}
+
+      <ul className={styles.speakers_product_list}>
+      {
+        categoryItems.map((item) => (
+          <li key={item.id} onClick={() => {
+            navigate(`/speakers/${item.id}`)
+          }}>
+            <Link>
+              <img src={item.image} alt={item.name}/>
+              <ul className={styles.product_color}>
+              {
+                item.colors.map((color, index) => (
+                  <li key={index}>
+                    <button 
+                      className={
+                        color === 'black' ? styles.blackButton 
+                        : color === 'white' ? styles.whiteButton 
+                        : color === 'brown' ? styles.brownButton 
+                        : color === 'cream' ? styles.creamButton 
+                        : styles.defaultButton
+                      }>
+                        {color}
+                    </button>
+                  </li>
+                ))
+              }
+              </ul>
+              <div className={styles.product_text_box}>
+                <span>{item.name}</span>
+                <p>$ {item.price}</p>
+              </div>
+            </Link>
+          </li>
+        ))
+      }
+      </ul>
+
+      {/* 변경 끝 */}
+
+{/* 
       <ul className={styles.speakers_product_home_list}>
       {
         categoryItems.filter((item) => item.category === 'HOME').map((item) => (
@@ -79,6 +125,7 @@ export default function Speakerslist() {
       }
       </ul>
 
+
       <div className={styles.speakers_product_box}>
         <ul className={styles.speakers_product_portable_list}>
         {
@@ -104,11 +151,51 @@ export default function Speakerslist() {
           ))
         }
         </ul>
-      </div>
+      </div> 
+*/}
+
     </section>      
+{/* 변경 시작 */}
 
     {
-      isPortableCategory && (
+      isPortableCategory && 
+      (
+        <div className={styles.speakers_promo}>
+          <p>Legendary sound, built for the road</p>
+          <span>The latest member of our portable line-up has arrived</span>
+        </div>
+      )
+    }
+
+    {
+      (isAllCategory || isLimitedCategory) && 
+      (
+        // 하나의 section만 만드는 거면 map으로 안 돌려도 될 것 같다.
+        // categoryItems.filter((item) => item.category === 'LIMITED EDITION').map((item) => (
+          <Link to={`/speakers/${categoryItems.id}`}>
+            <section className={styles.limited_edition}>
+              <div className={styles.limited_edition_box}>
+                <div className={styles.limited_edition_img}>
+                  <span>LIMITED<i>EDITION</i></span>
+                  <img src='/images/plp-diamond-jubilee-emberton-v2.png' alt='' />
+                </div>
+                <div className={styles.limited_edition_text}>
+                  <span>EMBERTON<br/>DIAMOND JUBILEE</span>
+                  <p>$179.99</p>
+                </div>
+              </div>
+            </section>
+          </Link>
+        // ))
+      )
+    } 
+
+{/* 변경 끝 */}
+
+
+    {/* {
+      isPortableCategory && 
+      (
         <div className={styles.speakers_promo}>
           <p>Legendary sound, built for the road</p>
           <span>The latest member of our portable line-up has arrived</span>
@@ -133,7 +220,7 @@ export default function Speakerslist() {
           </section>
         </Link>
       ))
-    } 
+    }  */}
 
     </>
   )
